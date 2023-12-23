41a492171b23ce6f6c5441b7ef07349d454b92f5
Tharushirdtk <kavindirdtk@gmail.com>
2023-12-23 22:37:16 +0530

    product

---
+++ include/product.php
@@ -0,0 +1,7 @@
+<?php
+if(!empty($_GET["action"])) 
+{
+$productId = isset($_GET['id']) ? htmlspecialchars($_GET['id']) : '';
+$quantity = isset($_POST['quantity']) ? htmlspecialchars($_POST['quantity']) : '';
+
+switch($_GET["action"])
