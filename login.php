7240e22d9fa874343fba2aa7c2deb39ab12159ca
mhnkarunarathna <hirushanuwanpriya17@gmail.com>
2023-12-23 22:04:01 +0530

    login 4

--- login.php
+++ login.php
@@ -61,6 +61,33 @@
 <div style=" background-image: url('images/img/pimg.jpg');">
  
         <?php
+
+include("connection/connect.php"); 
+error_reporting(0); 
+session_start(); 
+if(isset($_POST['submit']))  
+{
+	$username = $_POST['username'];  
+	$password = $_POST['password'];
+	if(!empty($_POST["submit"]))   
+     {
+	$loginquery ="SELECT * FROM users WHERE username='$username' && password='$password'";
+	$result=mysqli_query($db, $loginquery);
+	$row=mysqli_fetch_array($result);
+	                        if(is_array($row)) 
+								{
+                                    	$_SESSION["user_id"] = $row['u_id']; 
+										 header("refresh:1;url=index.php"); 
+	                            } 
+							else
+							    {
+                                      	$message = "Invalid Username or Password!"; 
+                                }
+	 }
+
+}
+?>
+has context menu
 has context menu
 
 
