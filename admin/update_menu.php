3818dc96f01f531eb98a1cb8fe4c06177ad85685
onethdilvan24 <onethdikkumbura@gmail.com>
2023-12-24 14:52:02 +0530

    update menu coding 1

---
+++ admin/update_menu.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+                <html lang="en">
+                <?php
+include("../connection/connect.php");
+error_reporting(0);
+session_start();
+
+
+
+
+if(isset($_POST['submit']))         
+{
+	
+			
+		
+			
+		  
+		
+		
+		if(empty($_POST['d_name'])||empty($_POST['about'])||$_POST['price']==''||$_POST['res_name']=='')
+		{	
+											$error = 	'<div class="alert alert-danger alert-dismissible fade show">
+																<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+																<strong>All fields Must be Fillup!</strong>
+															</div>';
+									
+		
+								
+		}
+	else
+		{
+		
+				$fname = $_FILES['file']['name'];
+								$temp = $_FILES['file']['tmp_name'];
+								$fsize = $_FILES['file']['size'];
+								$extension = explode('.',$fname);
+								$extension = strtolower(end($extension));  
+								$fnew = uniqid().'.'.$extension;
+   
+								$store = "Res_img/dishes/".basename($fnew);                     
+	
+					if($extension == 'jpg'||$extension == 'png'||$extension == 'gif' )
+					{        
+									if($fsize>=1000000)
+										{
+		
+		
+												$error = 	'<div class="alert alert-danger alert-dismissible fade show">
+																<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+																<strong>Max Image Size is 1024kb!</strong> Try different Image.
+															</div>';
+	   
+										}
+		
+									else
