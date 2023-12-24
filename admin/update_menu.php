95dc9f39e237404dcc77c5d5e61e6abcc9959819
onethdilvan24 <onethdikkumbura@gmail.com>
2023-12-24 14:57:09 +0530

    update menu coding sess 8

--- admin/update_menu.php
+++ admin/update_menu.php
@@ -274,3 +274,26 @@ if(isset($_POST['submit']))
                                     </div>
                                 </div>
                             </div>
+
+                            <?php include "include/footer.php" ?>
+
+                        </div>
+
+                    </div>
+
+                    </div>
+
+                    </div>
+
+                    <script src="js/lib/jquery/jquery.min.js"></script>
+                    <script src="js/lib/bootstrap/js/popper.min.js"></script>
+                    <script src="js/lib/bootstrap/js/bootstrap.min.js"></script>
+                    <script src="js/jquery.slimscroll.js"></script>
+                    <script src="js/sidebarmenu.js"></script>
+                    <script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
+                    <script src="js/custom.min.js"></script>
+
+                </body>
+
+
+                </html>
