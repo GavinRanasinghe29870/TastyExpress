f3408cae48a2496f2d1161c35481c1b94e442d91
onethdilvan24 <onethdikkumbura@gmail.com>
2023-12-24 14:53:32 +0530

    update menu coding sess 3

--- admin/update_menu.php
+++ admin/update_menu.php
@@ -87,3 +87,27 @@ if(isset($_POST['submit']))
 
 
 
+?>
+               
+
+                <head>
+                    <meta charset="utf-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <meta name="description" content="">
+                    <meta name="author" content="">
+                    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+                    <title>Update Menu</title>
+                    <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+                    <link href="css/helper.css" rel="stylesheet">
+                    <link href="css/style.css" rel="stylesheet">
+
+                </head>
+
+                <body class="fix-header">
+
+                    <div class="preloader">
+                        <svg class="circular" viewBox="25 25 50 50">
+                            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
+                        </svg>
+                    </div>
