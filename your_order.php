397f0b906013051b1cb8f2a535d0155e1059ef69
Liyanagelsa <sandaliashinsana0906@gmail.com>
2024-01-17 16:16:24 +0530

    Added the your order part

--- your_order.php
+++ your_order.php
@@ -237,3 +237,23 @@ else
             </div>
     </div>
     </section>
+
+    <?php include "include/footer.php" ?>
+
+    </div>
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
+<?php
+}
+?>
